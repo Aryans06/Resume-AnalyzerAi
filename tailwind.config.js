bf0c4cdb3c216eda62a6c7092e527d53f7890d5c
Aryan Jha <aryans2637@gmail.com>
2026-01-01 13:39:06 +0530

    Still setting up

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./app/**/*.{js,ts,jsx,tsx}", "./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
